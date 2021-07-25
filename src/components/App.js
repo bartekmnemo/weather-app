0bed58aa240e668888b30fee993e5bd614b6b2c7
St33v <48390535+bartekmnemo@users.noreply.github.com>
2021-07-25 21:34:26 +0200

    add fetch() and display data on results components

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,33 @@ import Form from './Form'
 import Result from './Result'
 import './App.css';
 
-const API = "c84b8f5c077147fb55d9a448295d1fe7"
 // API from https://home.openweathermap.org
+const ApiKey = "c84b8f5c077147fb55d9a448295d1fe7"
 
 class App extends Component {
   state = { 
     value: '',
-    sended: false,
+    date: '',
+    timezone: '',
+    country: '',
+    city: '',
+    sunrise: '',
+    sunset: '', 
+    temp: '',
+    feels_like: '',
+    temp_min: '',
+    temp_max: '',
+    humidity: '',
+    pressure: '', 
+    wind_speed: '',
+    wind_deg: '',
+    weather_main: '',
+    weather_description: '',
+    error: false
    }
 
+
   handleValueChange = e => {
-    console.log(e.target.value)
     this.setState({
       value: e.target.value,
     })
@@ -21,16 +37,53 @@ class App extends Component {
 
   handleCityClick = e => {
     e.preventDefault()
-    this.setState({
-      sended: true
+    const city = this.state.value
+    const API = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ApiKey}`
+    fetch(API)
+    .then( response => {
+      if(response.ok) {
+        return response.json()
+      }
+      throw Error(`Niepoprawna nazwa miasta`)
+      })
+    .then( data => {
+      const time = new Date().toLocaleString()
+      const weatherIndex = data.weather.length - data.weather.length
+      console.log(time)
+      this.setState( prevState => ({
+        date: time,
+        timezone: data.timezone,
+        country: data.sys.country,
+        city: data.name,
+        sunrise: data.sys.sunrise,
+        sunset: data.sys.sunset, 
+        temp: data.main.temp,
+        feels_like: data.main.feels_like,
+        temp_min: data.main.temp_min,
+        temp_max: data.main.temp_max,
+        humidity: data.main.humidity,
+        pressure: data.main.pressurp, 
+        wind_speed: data.wind.speed,
+        wind_deg: data.wind.deg,
+        weather_main: data.weather[weatherIndex].main,
+        weather_description: data.weather[weatherIndex].description,
+        error: false
+      }))
     })
+    .catch (err => {
+      console.log(err)
+      this.setState({
+        error: true
+      })
+    })
+    console.log(this.state.city)
   }
 
   render() { 
     return ( 
       <div className="app">
         <Form value={this.state.value} change={this.handleValueChange} click={this.handleCityClick}/>
-        <Result sended={this.state.sended} all={this.state}/>
+        <Result all={this.state}/>
       </div>
      );
   }
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,12 +1,35 @@
 import React from 'react';
 
 const Result = props => {
-    const {sended, value} = props.all
+    const {value, date, timezone, country, city, sunrise, sunset ,temp ,feels_like ,temp_min ,temp_max ,humidity ,pressure ,wind_speed ,wind_deg ,weather_main ,weather_description , error} = props.all
+    let content = null
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
+    if(!error && city){
+        const celcTemp = Math.round(temp - 273.15)
+        const sunriseTime = new Date((sunrise + timezone - 7200)  * 1000).toLocaleTimeString();
+        const sunsetTime = new Date((sunset + timezone - 7200) * 1000).toLocaleTimeString();
+        const windKM = Math.round((wind_speed / 1000) * 3600)
+        content = (
+            <div className="result">
+            <h3>Search results for <em>{capitalizeFirstLetter(city)}</em> ({country})</h3>
+            <h4>The current day and time: {date}</h4>
+            <h4>Temperature: {celcTemp} &#176;C </h4>
+            <h4>Weather conditions: {weather_main}, details: {weather_description}</h4> 
+            <h4>Sunrise today at: {sunriseTime}</h4>
+            <h4>Sunset today at: {sunsetTime}</h4>
+            <h4>Wind speed: {windKM} km/s</h4>
+            <h4>Pressure: {pressure} hPa</h4>
+        </div>
+        )
+    }
     return ( 
         <>
-        {sended && value}
+             {error ? <p>Incorrect city name: <span>{value}</span></p> : content}
         </>
      );
 }
+
  
 export default Result;
