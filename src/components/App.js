42ef21543360844f552eaca1f2184ab5f6530a27
St33v <48390535+bartekmnemo@users.noreply.github.com>
2021-07-25 17:36:47 +0200

    remove unnecessary files

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <>
+        <p>First App</p>
+      </>
+     );
+  }
+}
+ 
+export default App;
